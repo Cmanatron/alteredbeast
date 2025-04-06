bf053296cb41dbeb9609382b5d580449e10c1a9f
Clancy M <Cmanatron@gmail.com>
2025-04-06 02:03:24 -0400

    Saving Current Code
    I just be pushing shit

--- enemy.gd
+++ enemy.gd
@@ -6,15 +6,14 @@ extends CharacterBody2D
 @export var ySpeed = 0
 var animalID = [0,0,0,0,0]
 
+
 var maxSpeed = 400
 var maxJump = 400
 var slowX = 10
 var slowY = 10
-var health = 10
-
-func _ready() -> void:
-	add_to_group("Player")
-
+var damage = 1
+var atk = false
+var health = 3;
 func get_input():
 	if(Input.is_action_pressed('MoveRight')):
 		speed = maxSpeed;
@@ -26,6 +25,7 @@ func get_input():
 		#print('jump')
 	if(Input.is_action_pressed("Attack")):
 		$FrontLegs.angular_velocity= -90
+		
 		#self.scale.x = -1;
 	
 	
@@ -33,17 +33,19 @@ func get_input():
 func _physics_process(delta):
 	$Head.rotation = 0;
 	#print('Moving')
-	get_input()
+	#get_input()
 	velocity.x = speed
 	velocity.y = ySpeed
 	print($FrontLegs.get_contact_count())
 	
 	if($FrontLegs.get_colliding_bodies().has($Floor)):
 		ySpeed =0 
-		#print('Contact')
+		print('Contact')
 	if($BackLegs.get_colliding_bodies().has($Floor)):
 		ySpeed =0 
-		#print('Contact')
+		print('Contact')
+		if(health ==0 ):
+			get_tree().change_scene_to_file("res://intermission.tscn")
 	
 		
 	else:
@@ -59,6 +61,7 @@ func _physics_process(delta):
 
 
 func _on_hurt_box_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Enemy"):
-		print('Damage Taken Player')
-	health -= 1;
+	if area.is_in_group("Player"):
+		print('Damage Taken Enemy')
+		health -= 1;
+		
